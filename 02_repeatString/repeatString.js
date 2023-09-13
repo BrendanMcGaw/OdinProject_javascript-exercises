2223ed766d088628731ce384616c15b0d10fd2f2
Brendan McGaw <Brendanmcgaw@gmail.com>
2023-09-13 21:58:27 +1000

    Edited the code to work, passed two tests so far, more to go

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,7 +1,9 @@
-const repeatString = function() {
-    word = "hey"
-    return word + word + word;
-};
+const repeatString = function(string, num) {
+    for (let i = 0; i < num -1; i++) {
+        string += "hey";
+    }
+    return string;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
