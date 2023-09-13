a57707cc6df8fb517f8984501255adf2cafbd361
Brendan McGaw <Brendanmcgaw@gmail.com>
2023-09-13 23:16:34 +1000

    First piece of written function works and passes test 1.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(words) {
+    const wordArray = [];
+    for (let i in words) {
+        // We're  creating an array from the arguments of the function, that will place each piece in the normal position for each character in the words.
+        wordArray.splice(words.length, 0, words[i]);
+        console.log(wordArray);
+    }
+    // Reverses the order of the letters in the array and joins them all together using "nospaces"
+    reversedWordArray  = wordArray.reverse().join("");
+    console.log(reversedWordArray);
+    return reversedWordArray;
 };
 
 // Do not edit below this line
