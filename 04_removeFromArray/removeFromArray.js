90e919c09142ee56e6deee0482e35e8af7971452
Brendan McGaw <Brendanmcgaw@gmail.com>
2023-09-15 00:49:32 +1000

    Got my  argument targetting to work with finding the indexOf a value, assosciated with another argument. Just need to add it all to a loop to iterate through whatever arguments they might have and assosciate those elements with an index to splice them out.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array) {
+    // Is finding the index number of the element in the second argument 
+    // of the function in argument[0]
+    // Since arguments[0] = array
+    // arguments[1] = item to be excluded.
+    let itemsToRemove = arguments[0].indexOf(arguments[1]);
+    array.splice(itemsToRemove, 1)
+    return array;
+    // // When  logging  arguments, you can see the placement of each argument,
+    // // Therefore we can say rather definitively that anything inside an array
+    // // As an argument would be considered 1 argument.
+    // // Each , after represents another argument.
+    // console.log(array)
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
