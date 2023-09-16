b2bb03dd21f382d03d9c94655bfdff1ec6acf4e4
Brendan McGaw <Brendanmcgaw@gmail.com>
2023-09-17 00:04:39 +1000

    Got the code to work and pass all tests, a little longer then I wanted but can always refactor later

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, lastNumber) {
+    let result = 0;
+    let i = firstNumber;
+    // Whilever the first argument is larger then the second argument, it will do this while loop to add all the numbers.
+    // typeof operator allows me to check if what data type is being used.
+    if ((firstNumber || lastNumber) < 0 || typeof firstNumber !== 'number' || typeof lastNumber !== 'number') {
+        return "ERROR";
+    };
+    // If firstnumber is larger then last number, it will  iterate down by 1 per time.
+    if (firstNumber > lastNumber) {
+        while(i >= lastNumber) {
+            result = result + i;
+            i--;
+        };
+    return result;
+    };
 
+    // Whilever the first argument is smaller then the second argument, it will loop through each number adding them all in summary.
+    if (firstNumber < lastNumber) {
+        while(i <= lastNumber) {
+            result = result + i;
+            i++;
+        };
+    return result;
+    };
 };
 
 // Do not edit below this line
