88353d014e6416f66a2e461c8fd813546b033203
Brendan McGaw <Brendanmcgaw@gmail.com>
2023-09-17 23:33:41 +1000

    Added functionality to the factorial function, allowing any calculation to be made.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -34,8 +34,28 @@ const power = function(number, exponent) {
   return number**exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	// Computes the factorial of an input. A factorial is every numbers between the one provided and 0, 
+  // multiplied by one another.
+  // Factorial of 0 = 1, something about positive numbers.
+  let result = number;
+  let i = number-1;
+  // We set i = number-1 because we need i to start at 1 number lower then the original.
+  // This if statements simply returns 1 if the number input is 0, as per factorial rules.
+  if (number == 0){
+    return  result = 1;
+  }
+  // our while loop will break off i hits 0 because a multiple of 0 would result in a 0 product answer.
+  while (i > 0) {   
+    if (i == 0){
+      return result;
+    }
+    // Example. Result = 5 * i-- (4 because of our i = number-1) and then takes 1  away, makeing it 3
+    // for the next go round.
+    result *= i--;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
