b8c2d2c4f91f44f4c60147118cdc6bc79a3ecb51
Brendan McGaw <Brendanmcgaw@gmail.com>
2023-09-17 22:34:34 +1000

    Created 2 functions that allow conversion between fahrenheit and celsius, rounding to 1 decimal place and parsing the resulted string as a float

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,33 @@
-const convertToCelsius = function() {
+// Converts Fahrenheit to Celsius
+const convertToCelsius = function(temperature) {
+  let celsius;
+  // Makes  sure that the input was a number.
+  if (typeof temperature  == 'number') {
+    // Takes the mathematical formula to convert to Celsius and fixes the decimal place to 1.
+    celsius = ((temperature - 32) * (5/9)).toFixed(1);
+    console.log(celsius);
+    // parseFloat allows the string created by toFixed, to become a float.
+    return parseFloat(celsius);
+  }
 };
-
-const convertToFahrenheit = function() {
-};
+// Converts Celsius to Fahrenheit.
+const convertToFahrenheit = function(temperature) {
+  let fahrenheit;
+  // Makes surre that the input was a number.
+  if (typeof temperature == "number") {
+    // Does the mathematical conversion and sets it to a fixed decimal place of 1.
+    fahrenheit = ((temperature * (9/5)) + 32).toFixed(1);
+    console.log(fahrenheit);
+    // parseFloat allows us to convert our result to a float, from a string.
+    return parseFloat(fahrenheit);
+  };
+}
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
+// Conversion Maths from Celsius to Fahrenheit is::
+// (Celsius * 9/5) + 32 = Fahrenheit.
