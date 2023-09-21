204a44daef3c5924e9daf9615ebd12266066fddf
Brendan McGaw <Brendanmcgaw@gmail.com>
2023-09-22 00:27:14 +1000

    Added a for loop, that iterates through the array of elements in the object and pushes the titles of each book to a seperate array and returns those values

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,21 @@
-const getTheTitles = function() {
 
+
+const getTheTitles = function() {
+    const books = [
+        {
+          title: 'Book',
+          author: 'Name'
+        },
+        {
+          title: 'Book2',
+          author: 'Name2'
+        }
+      ]
+    const bookTitles = [];
+    for (let i = 0; i < books.length; i++) {
+        bookTitles.push(books[i].title);
+    }
+    return bookTitles;
 };
 
 // Do not edit below this line
