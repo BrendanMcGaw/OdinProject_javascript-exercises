2a603c1af8a949a8aa163006f2c410426c003b45
Brendan McGaw <Brendanmcgaw@gmail.com>
2023-09-21 21:01:13 +1000

    Trying to get regular expressions to work

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,16 +1,17 @@
 const leapYears = function(year) {
+    // If year is divisible by 100 and 400. Return true.
     if (year % 100 == 0 && year % 400 == 0) {
         return true;
     }
-
+    // If year is divisible by 4 and 100 but NOT by 400. Return false.
     if (year % 4 == 0 && year % 100 == 0 && year % 400 !== 0) {
         return false;
     }
-    
+    // If year is divisible by 4 (set after checking the other information) return true;
     if (year % 4 == 0) {
         return true;
     }
-
+    // Any other  circumstance should return false.
     else {
         return false;
     }
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+    const searchFor = /[a-zA-Z]/g
+    const newArray = input.split(searchFor); // [input, ","]
+    // splits the input string into individual elements and assigns them an index value in an array.
+    console.log(newArray);
+    // reversed the index values of the array
+    let reverseArray = newArray.reverse();
+    console.log(reverseArray);
+    // Joins the elements of the array together back into a string. "" allows it to remove the commas between each element in the string.
+    let reversedInput = reverseArray.join("");
+    console.log(reversedInput);
+    // Checks to see if the reversed version of the original input is a true palindrome.
+    if (input == reversedInput) {
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
